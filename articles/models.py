285617084b6e6a8b584f2622804d0b4c8022815f
Jacky <964413011@qq.com>
2021-03-13 15:55:08 +0800

    article image model support

--- articles/models.py
+++ articles/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from taggit.managers import TaggableManager
 from users.models import UserInformation
 from django.utils.translation import gettext_lazy as _
+from  django.utils import timezone
 # Create your models here.
 
 
@@ -11,7 +12,10 @@ class ArticleModel(models.Model):
     title = models.CharField("title", max_length=50)
     author = models.ForeignKey(
         UserInformation, verbose_name="user", on_delete=models.CASCADE)
-    body = models.FileField("file", upload_to="articels/", max_length=100)
+    body = models.FileField("file", upload_to="articles/markdown", max_length=100)
+
+    createTime=models.DateTimeField(_("create time"), auto_now=False, auto_now_add=True)
+    modifyTime=models.DateTimeField(_("modify time"), auto_now=True, auto_now_add=False)
 
     tags = TaggableManager()
 
@@ -24,3 +28,18 @@ class ArticleModel(models.Model):
 
     def get_absolute_url(self):
         return reverse("articlemodel_detail", kwargs={"pk": self.pk})
+
+class ArticleImage(models.Model):
+
+    image = models.ImageField(_("image"), upload_to="articles/image", height_field=None, width_field=None, max_length=100)
+    article=models.ForeignKey(ArticleModel, verbose_name=_(""), on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("articleimage")
+        verbose_name_plural = _("articleimages")
+
+    def __str__(self):
+        return self.image.__str__()
+
+    def get_absolute_url(self):
+        return reverse("articleimage_detail", kwargs={"pk": self.pk})
